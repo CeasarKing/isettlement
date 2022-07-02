090233dadb77e9c3d7b7c65ef84a24b9168bf440
linchaocong <1083895682@qq.com>
2022-07-02 12:14:11 +0800

    初始化项目

---
+++ config-center/src/main/java/com/lin/isettlement/configcenter/IsettlementApplication.java
@@ -0,0 +1,13 @@
+package com.lin.isettlement.configcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsettlementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsettlementApplication.class, args);
+	}
+
+}
